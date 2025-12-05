2239094a6c63a5e8a3d3e6a5dcffd88c780fca72
AmalDhouib <157402517+AmalDhouib@users.noreply.github.com>
2025-12-05 10:14:56 +0100

    Update BarChart.tsx

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -5,16 +5,28 @@ import { useTranslation } from 'react-i18next';
 const BarChart = () => {
   const { t } = useTranslation();
 
-  const [data] = useState([
-    { name: 'A', value: 50 },
-    { name: 'B', value: 20 },
-    { name: 'C', value: 40 },
-    { name: 'D', value: 70 },
-  ]);
+  const [data, setData] = useState([
+    {
+      name: 'A',
+      value: 50,
+    },
+    {
+      name: 'B',
+      value: 20,
+    },
+    {
+      name: 'C',
+      value: 40,
+    },
+    {
+      name: 'D',
+      value: 70,
+    },
 
   useEffect(() => {
     const w = 960 / 2;
     const h = 500 / 2;
+-
     const margin = { top: 20, right: 20, bottom: 30, left: 40 };
     const width = w - margin.left - margin.right;
     const height = h - margin.top - margin.bottom;
@@ -30,8 +42,17 @@ const BarChart = () => {
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-    x.domain(data.map(d => d.name));
-    y.domain([0, d3.max(data, d => d.value) ?? 0]);
+    x.domain(
+      data.map(function (d) {
+        return d.name;
+      })
+    );
+    y.domain([
+      0,
+      d3.max(data, function (d) {
+        return d.value;
+      }) ?? 0,
+    ]);
 
     svg
       .selectAll('.bar')
@@ -39,21 +60,27 @@ const BarChart = () => {
       .enter()
       .append('rect')
       .attr('class', 'bar')
-      .attr('x', d => x(d.name) ?? 0)
+      .attr('x', function (d) {
+        return x(d.name) ?? 0;
+      })
       .attr('width', x.bandwidth())
-      .attr('y', d => y(d.value))
-      .attr('height', d => height - y(d.value));
+      .attr('y', function (d) {
+        return y(d.value);
+      })
+      .attr('height', function (d) {
+        return height - y(d.value);
+      });
+
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x));      .attr('height', d => height - y(d.value));
 
     svg.append('g').attr('transform', 'translate(0,' + height + ')').call(d3.axisBottom(x));
     svg.append('g').call(d3.axisLeft(y));
   }, [data]);
 
-  return (
-    <div>
-      <h2 className="text-xl font-semibold mb-4">{t('chartTitle')}</h2>
-      <div className="bar-chart"></div>
-    </div>
-  );
+  return <div className="bar-chart"></div>;
 };
 
 export default BarChart;
